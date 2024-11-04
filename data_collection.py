f0b335e108fc59933fcb307b263fdf83cbbe5687
atpower89 <124506029+atpower89@users.noreply.github.com>
2024-11-04 17:06:13 -0500

    Add files via upload

---
+++ data_collection.py
@@ -0,0 +1,39 @@
+import generating_Jij as gj
+import numpy as np
+import argparse
+import time
+
+def run_simulation(N, numSeeds):
+    nString = str(N) + "_" + str(numSeeds) + ".txt"
+    with open(nString, "a") as f:
+        start = time.time()
+        f.write(str(N) + "-Dimensional System - Ground States (seed, configuration)")
+        f.write("\n")
+
+        for seed in range(0,numSeeds):
+            Jij = gj.generateJij(N, seed=seed)
+            bm = gj.biqMac(Jij)
+
+            bmList = []
+            for element in bm:
+                bmList.append(gj.binary_to_decimal(element))
+            bmString = str(seed) + ": " + str(bmList)
+
+            np.savetxt(f, [bmString], fmt = '%s', delimiter=',')
+
+        end = time.time()
+
+        f.write("Computational time: " + str(end-start) + "s")
+        f.write("\n")
+        f.write("\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Jij ground state simulation")
+    parser.add_argument("-N", type=int, required=True, help="Maximum system size")
+    parser.add_argument("-numSeeds", type=int, required=True, help="Number of seeds for random generation")
+    
+    args = parser.parse_args()
+    
+    # Call main function with arguments
+    run_simulation(args.N, args.numSeeds)
+
